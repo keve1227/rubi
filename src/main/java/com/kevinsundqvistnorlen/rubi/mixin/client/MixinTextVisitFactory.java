b01ae2ea9a07f0ea4648a2c3179339d34ec018cb
Keve1227 <kevin.sundqvistnorlen@gmail.com>
2023-09-10 16:01:25 +0200

    Allow ruby annotations in the format `§^text (ruby)`

---
+++ src/main/java/com/kevinsundqvistnorlen/rubi/mixin/client/MixinTextVisitFactory.java
@@ -0,0 +1,17 @@
+package com.kevinsundqvistnorlen.rubi.mixin.client;
+
+import net.minecraft.text.TextVisitFactory;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.*;
+
+@Mixin(TextVisitFactory.class)
+public class MixinTextVisitFactory {
+
+    @ModifyVariable(
+        method = "visitFormatted(Ljava/lang/String;ILnet/minecraft/text/Style;Lnet/minecraft/text/Style;" +
+                 "Lnet/minecraft/text/CharacterVisitor;)Z", at = @At("HEAD"), argsOnly = true
+    )
+    private static String visitFormatted(String string) {
+        return string.replaceAll("§\\^\\s*(.+?)\\s*\\(\\s*(.+?)\\s*\\)", "\ue9c0$1\ue9c1$2\ue9c2");
+    }
+}
