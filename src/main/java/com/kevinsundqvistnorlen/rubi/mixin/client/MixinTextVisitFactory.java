3e679a82112fb5e8c372076835c073d1cda5d003
Venroy <64047882+VenroyDEV@users.noreply.github.com>
2024-05-24 19:34:06 +0200

    whoops fixxed, fixxed the remapping folder

--- src/main/java/com/kevinsundqvistnorlen/rubi/mixin/client/MixinTextVisitFactory.java
+++ src/main/java/com/kevinsundqvistnorlen/rubi/mixin/client/MixinTextVisitFactory.java
@@ -8,11 +8,11 @@ import org.spongepowered.asm.mixin.injection.*;
 public class MixinTextVisitFactory {
 
     @ModifyVariable(
-        method = "visitFormatted(Ljava/lang/String;ILnet/minecraft/text/Style;Lnet/minecraft/text/Style;" +
-                 "Lnet/minecraft/text/CharacterVisitor;)Z", at = @At("HEAD"), argsOnly = true
+            method = "visitFormatted(Ljava/lang/String;ILnet/minecraft/text/Style;Lnet/minecraft/text/Style;" +
+                    "Lnet/minecraft/text/CharacterVisitor;)Z", at = @At("HEAD"), argsOnly = true
     )
     private static String visitFormatted(String string) {
         return string.replaceAll("[\ue9c0-\ue9c2]", "")
-            .replaceAll("§\\^\\s*(.+?)\\s*\\(\\s*(.+?)\\s*\\)", "\ue9c0$1\ue9c1$2\ue9c2");
+                .replaceAll("§\\^\\s*(.+?)\\s*\\(\\s*(.+?)\\s*\\)", "\ue9c0$1\ue9c1$2\ue9c2");
     }
 }
