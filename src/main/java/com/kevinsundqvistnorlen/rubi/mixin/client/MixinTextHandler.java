3335f2977e2e6a945b74294b736fcd1b6fed8ad8
Kevin Sundqvist Norlén <kevin.sundqvistnorlen@gmail.com>
2025-02-09 19:20:42 +0100

    Fix settings screen title not being centered
    (Due to me breaking the width receiver mixin with 1cec4076719e01f0a29b8376dd2effb50faded47)

--- src/main/java/com/kevinsundqvistnorlen/rubi/mixin/client/MixinTextHandler.java
+++ src/main/java/com/kevinsundqvistnorlen/rubi/mixin/client/MixinTextHandler.java
@@ -1,70 +1,72 @@
 package com.kevinsundqvistnorlen.rubi.mixin.client;
 
 import com.kevinsundqvistnorlen.rubi.IRubyStyle;
-import com.kevinsundqvistnorlen.rubi.OrderedTextStringVisitable;
+import com.llamalad7.mixinextras.sugar.Local;
+import com.llamalad7.mixinextras.sugar.ref.LocalRef;
 import net.minecraft.client.font.TextHandler;
 import net.minecraft.text.*;
 import org.apache.commons.lang3.mutable.MutableFloat;
 import org.spongepowered.asm.mixin.*;
-import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-
-import java.util.Optional;
 
 @Mixin(TextHandler.class)
 public abstract class MixinTextHandler {
     @Unique private final ThreadLocal<Boolean> recursionGuard = ThreadLocal.withInitial(() -> false);
 
-    @ModifyVariable(
-        method = "<init>",
-        argsOnly = true,
-        at = @At(
-            value = "FIELD",
-            target = "Lnet/minecraft/client/font/TextHandler;" +
-                "widthRetriever:Lnet/minecraft/client/font/TextHandler$WidthRetriever;"
-        )
-    )
-    private TextHandler.WidthRetriever modifyWidthRetrieverConstructor(
-        TextHandler.WidthRetriever widthRetriever
+    @Inject(method = "<init>", at = @At("CTOR_HEAD"))
+    private void onTextHandlerInit(
+        TextHandler.WidthRetriever widthRetriever, CallbackInfo ci,
+        @Local(argsOnly = true) LocalRef<TextHandler.WidthRetriever> localWidthRetriever
     ) {
-        return (codePoint, style) -> IRubyStyle
+        localWidthRetriever.set((codePoint, style) -> IRubyStyle
             .getRuby(style)
-            .map((rubyText -> rubyText.getWidth((TextHandler) (Object) this)))
-            .orElseGet(() -> widthRetriever.getWidth(codePoint, style));
+            .map(rubyText -> rubyText.getWidth((TextHandler) (Object) this))
+            .orElseGet(() -> widthRetriever.getWidth(codePoint, style)));
     }
 
     @Shadow
     public abstract float getWidth(StringVisitable text);
 
+    @Unique
+    public abstract float hello();
+
+    @Shadow
+    public abstract float getWidth(OrderedText text);
+
+    @Inject(method = "getWidth(Ljava/lang/String;)F", at = @At("HEAD"), cancellable = true, order = 900)
+    private void onGetWidth(String text, CallbackInfoReturnable<Float> cir) {
+        this.onGetWidth(visitor -> TextVisitFactory.visitFormatted(text, Style.EMPTY, visitor), cir);
+    }
+
     @Inject(
-        method = "getWidth(Lnet/minecraft/text/StringVisitable;)F",
-        at = @At("HEAD"),
-        cancellable = true,
-        order = 900
+        method = "getWidth(Lnet/minecraft/text/StringVisitable;)F", at = @At("HEAD"), cancellable = true, order = 900
     )
-    public void onGetWidth(StringVisitable text, CallbackInfoReturnable<Float> cir) {
+    private void onGetWidth(StringVisitable text, CallbackInfoReturnable<Float> cir) {
+        this.onGetWidth(visitor -> TextVisitFactory.visitFormatted(text, Style.EMPTY, visitor), cir);
+    }
+
+    @Inject(method = "getWidth(Lnet/minecraft/text/OrderedText;)F", at = @At("HEAD"), cancellable = true, order = 900)
+    private void onGetWidth(OrderedText text, CallbackInfoReturnable<Float> cir) {
         if (this.recursionGuard.get()) return;
         this.recursionGuard.set(true);
 
         try {
             var width = new MutableFloat();
-            text.visit((style, string) -> {
+            text.accept((index, style, codePoint) -> {
                 width.add(
                     IRubyStyle
                         .getRuby(style)
                         .map(ruby -> ruby.getWidth((TextHandler) (Object) this))
-                        .orElseGet(() -> this.getWidth(StringVisitable.styled(string, style)))
+                        .orElseGet(() -> this.getWidth(OrderedText.styled(codePoint, style)))
                 );
-                return Optional.empty();
-            }, Style.EMPTY);
+                return true;
+            });
             cir.setReturnValue(width.floatValue());
         } finally {
             this.recursionGuard.set(false);
         }
-    }
-
-    @Inject(method = "getWidth(Lnet/minecraft/text/OrderedText;)F", at = @At("HEAD"), cancellable = true, order = 900)
-    private void onGetWidth(OrderedText text, CallbackInfoReturnable<Float> cir) {
-        this.onGetWidth(new OrderedTextStringVisitable(text), cir);
     }
 }
